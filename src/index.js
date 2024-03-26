bacc6e8b1f8f9678dfb9fae17f80ab449089c7ce
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-03-26 16:20:20 +0100

    First test

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ const log = logger.child({
   module: 'main',
 });
 
+const puppeteer = require('puppeteer');
+
 async function main() {
-  log.info('hello world');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.courdecassation.fr');
+
+  const searchResultSelector = '#block-derniersarretsblock';
+  const derniersArretsBlock = await page.$(searchResultSelector);
+
+  const searchArretSelector = '.arret';
+  const arretBlocks = await derniersArretsBlock.$$(searchArretSelector);
+
+  for (let i = 0; i < arretBlocks.length; i++) {
+    const searchDate = 'p.date';
+    const arretDate = await arretBlocks[i].$(searchDate);
+    const value = await arretDate.evaluate((el) => el.textContent);
+    log.info(value);
+  }
+
+  await browser.close();
 }
 
 main();
