d78eea00e9d1a84388cb808abeaf12617b1cd6e7
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-06-25 21:30:08 +0200

    Add details

--- src/probes/latest.js
+++ src/probes/latest.js
@@ -17,8 +17,53 @@ async function main() {
   log.info(latestFromAPI);
   if (JSON.stringify(latestFromBrowser) !== JSON.stringify(latestFromAPI)) {
     if (state === null || state.status === true) {
+      let details = [];
+      if (latestFromBrowser.length < latestFromAPI.length) {
+        for (let i = 0; i < latestFromAPI.length; i++) {
+          if (i < latestFromBrowser.length) {
+            if (JSON.stringify(latestFromAPI[i]) !== JSON.stringify(latestFromBrowser[i])) {
+              details.push(
+                `- [index ${i + 1}] Browser: \`${JSON.stringify(latestFromBrowser[i])}\` / API: \`${JSON.stringify(
+                  latestFromAPI[i],
+                )}\``,
+              );
+            }
+          } else {
+            details.push(`- [index ${i + 1}] Browser: \`empty\` / API: \`${JSON.stringify(latestFromAPI[i])}\``);
+          }
+        }
+      } else if (latestFromBrowser.length > latestFromAPI.length) {
+        for (let i = 0; i < latestFromBrowser.length; i++) {
+          if (i < latestFromAPI.length) {
+            if (JSON.stringify(latestFromAPI[i]) !== JSON.stringify(latestFromBrowser[i])) {
+              details.push(
+                `- [index ${i + 1}] Browser: \`${JSON.stringify(latestFromBrowser[i])}\` / API: \`${JSON.stringify(
+                  latestFromAPI[i],
+                )}\``,
+              );
+            }
+          } else {
+            details.push(`- [index ${i + 1}] Browser: \`${JSON.stringify(latestFromBrowser[i])}\` / API: \`empty\``);
+          }
+        }
+      } else {
+        if (latestFromBrowser.length > 0) {
+          for (let i = 0; i < latestFromBrowser.length; i++) {
+            if (JSON.stringify(latestFromAPI[i]) !== JSON.stringify(latestFromBrowser[i])) {
+              details.push(
+                `- [index ${i + 1}] Browser: \`${JSON.stringify(latestFromBrowser[i])}\` / API: \`${JSON.stringify(
+                  latestFromAPI[i],
+                )}\``,
+              );
+            }
+          }
+        } else {
+          details.push(`- Browser: \`all empty\` / API: \`all empty\``);
+        }
+      }
+      details = details.join('\n');
       await Slack.SendMessage(
-        ":large_red_square: les données du bloc des dernières décisions sur le site ne correspondent pas aux données de l'API",
+        `:large_red_square: Les données du bloc des dernières décisions sur le site ne correspondent pas aux données de l'API:\n${details}`,
       );
     }
     await State.SetState('latest', {
@@ -27,7 +72,7 @@ async function main() {
   } else {
     if (state === null || state.status === false) {
       await Slack.SendMessage(
-        ":large_green_square:	les données du bloc des dernières décisions sur le site correspondent de nouveau aux données de l'API",
+        ":large_green_square: Les données du bloc des dernières décisions sur le site correspondent de nouveau aux données de l'API.",
       );
     }
     await State.SetState('latest', {
