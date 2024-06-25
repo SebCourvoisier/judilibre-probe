592cb3cf633609021ee8ab84ac308c487b1c97b0
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-06-25 16:11:43 +0200

    Add state

---
+++ src/modules/state.js
@@ -0,0 +1,44 @@
+const { logger } = require('./logger');
+const log = logger.child({
+  module: 'State',
+});
+
+const path = require('path');
+const fs = require('fs/promises');
+
+class State {
+  static async GetState(key) {
+    try {
+      let state = null;
+      if (/^[a-z]+$/i.test(key) === false) {
+        throw new Error(`invalid key ${key}`);
+      }
+      const fileContent = (await fs.readFile(path.join(__dirname, '..', '..', 'state', `${key.json}`))).toString();
+      state = JSON.parse(fileContent);
+      return state;
+    } catch (e) {
+      log.error(e);
+      return null;
+    }
+  }
+
+  static async SetState(key, state) {
+    try {
+      if (/^[a-z]+$/i.test(key) === false) {
+        throw new Error(`invalid key ${key}`);
+      }
+      if (state === undefined || typeof state !== 'object') {
+        state = {};
+      }
+      state.time = new Date();
+      const fileContent = JSON.stringify(state, null, 2);
+      await fs.writeFile(path.join(__dirname, '..', '..', 'state', `${key.json}`), fileContent);
+      return true;
+    } catch (e) {
+      log.error(e);
+      return false;
+    }
+  }
+}
+
+exports.State = State;
--- src/probes/latest.js
+++ src/probes/latest.js
@@ -3,20 +3,36 @@ const log = logger.child({
   module: 'latest',
 });
 
+const { State } = require('../modules/state');
 const { Browser } = require('../modules/browser');
 const { API } = require('../modules/api');
 const { Slack } = require('../modules/slack');
 
 async function main() {
   log.info('start probe');
+  const state = await State.GetState('latest');
   const latestFromBrowser = await Browser.GetLatest();
   const latestFromAPI = await API.GetLatest();
   log.info(latestFromBrowser);
   log.info(latestFromAPI);
   if (JSON.stringify(latestFromBrowser) !== JSON.stringify(latestFromAPI)) {
-    await Slack.SendMessage(
-      ":warning: les données du bloc des dernières décisions sur le site ne correspondent pas aux données de l'API",
-    );
+    if (state === null || state.status === true) {
+      await Slack.SendMessage(
+        ":large_red_square: les données du bloc des dernières décisions sur le site ne correspondent pas aux données de l'API",
+      );
+    }
+    await State.SetState('latest', {
+      status: false,
+    });
+  } else {
+    if (state && state.status === false) {
+      await Slack.SendMessage(
+        ":large_green_square:	les données du bloc des dernières décisions sur le site correspondent de nouveau aux données de l'API",
+      );
+    }
+    await State.SetState('latest', {
+      status: true,
+    });
   }
   log.info('end probe');
 }
